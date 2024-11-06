ec8bc3ded6909bd992661b5948babb5413565171
Paula Rapkievicz <paularapkievicz@gmail.com>
2024-11-06 19:33:48 -0300

    Configurando o AdminJS

---
+++ src/adminjs/index.ts
@@ -0,0 +1,34 @@
+import AdminJS from 'adminjs'
+import AdminJsExpress from '@adminjs/express'
+import AdminJsSequelize from '@adminjs/sequelize' // aponta qual é o ORM
+import { sequelize } from '../database'
+
+AdminJS.registerAdapter(AdminJsSequelize) // aponta qual o adaptador do banco de dados
+
+export const adminJs = new AdminJS({
+  databases: [sequelize], // passa o banco de dados
+  rootPath: '/admin', 
+  branding: {
+    companyName: 'OneBitFlix',
+    logo: '/onebitflix.svg',
+    theme: {
+      colors: {
+        primary100: '#ff0043',
+	      primary80: '#ff1a57',
+	      primary60: '#ff3369',
+	      primary40: '#ff4d7c',
+		    primary20: '#ff668f',
+	      grey100: '#151515',
+	      grey80: '#333333',
+	      grey60: '#4d4d4d',
+	      grey40: '#666666',
+	      grey20: '#dddddd',
+	      filterBg: '#333333',
+	      accent: '#151515',
+	      hoverBg: '#151515',
+      }
+    }
+  }
+})
+
+export const adminJsRouter = AdminJsExpress.buildRouter(adminJs) //vai construir as rotas e inserir no app do express
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import express from 'express'
-import { database } from './database'
+import { sequelize } from './database'
+import { adminJs, adminJsRouter } from './adminjs'
 
 const app = express()
+
+app.use(express.static('public'))
+
+//app.use(caminho, rotas)
+app.use(adminJs.options.rootPath, adminJsRouter)
 
 const PORT = process.env.port || 3000
 
 app.listen(PORT, () => {
-  database.authenticate().then(() => {
+  sequelize.authenticate().then(() => {
     console.log('DB connection successfull.')
   })
 
