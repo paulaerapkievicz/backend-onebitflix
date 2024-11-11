b0cac25fda3776363037e7955b115771988a3f91
Paula Rapkievicz <paularapkievicz@gmail.com>
2024-11-11 12:39:34 -0300

    Obtendo 3 cursos em destaque

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,13 @@
 import express from 'express'
 import { categoriesController } from './controllers/categoriesController'
 import { coursesController } from './controllers/courseController'
-
+// oredem das rotas importa
 const router = express.Router()
 
 router.get('/categories', categoriesController.index)
 router.get('/categories/:id', categoriesController.show)
 
+router.get('/courses/featured', coursesController.featured)
 router.get('/courses/:id', coursesController.show)
 
 export { router }
