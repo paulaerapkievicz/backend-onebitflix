ef06f397c8c8b8be8ec0445f3d88902b8e297da2
Paula Rapkievicz <paularapkievicz@gmail.com>
2024-11-19 19:38:41 -0300

    Adicionando favoritos

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,18 +1,29 @@
 import { Category } from './Category'
 import { Course } from './Course'
 import { Episode } from './Episode'
+import { Favorite } from './Favorite'
 import { User } from './User'
 
 // aqui vou definir as associações entre cada tabela
 Category.hasMany(Course, { as: 'courses' }) // hasMany:uma categoria tem muitos cursos
-Course.belongsTo(Category) // belongsTo: um curso pertence a uma categoria
 
+Course.belongsTo(Category) // belongsTo: um curso pertence a uma categoria
 Course.hasMany(Episode, { as: 'episodes' })
+Course.belongsToMany(User, { through: Favorite })
+Course.hasMany(Favorite, { as: 'favoritesUsers', foreignKey: 'course_id' })
+
 Episode.belongsTo(Course)
+
+Favorite.belongsTo(Course)
+Favorite.belongsTo(User)
+
+User.belongsToMany(Course, { through: Favorite })
+User.hasMany(Favorite, { as: 'favoritesCourses', foreignKey: 'user_id' })
 
 export {
   Category,
   Course,
   Episode,
+  Favorite,
   User
 }
