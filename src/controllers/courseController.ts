ed6b85c86427cd41196ea8ed549426b5286974b7
Paula Rapkievicz <paularapkievicz@gmail.com>
2024-11-21 13:13:21 -0300

    Atualizando o endpoint de cursos

--- src/controllers/courseController.ts
+++ src/controllers/courseController.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express'
 import { courseService } from '../services/courseService'
 import { getPaginationParams } from '../helpers/getPaginationParams'
+import { AuthenticatedRequest } from '../middlewares/auth'
+import { likeService } from '../services/likeService'
+import { favoriteService } from '../services/favoriteService'
 
 export const coursesController = {
     // GET /courses/featured
@@ -10,7 +13,7 @@ export const coursesController = {
             return res.json(featuredCourses)
         } catch (err) {
             if (err instanceof Error) {
-            return res.status(400).json({ message: err.message })
+                return res.status(400).json({ message: err.message })
             }
         }
     },
@@ -18,12 +21,12 @@ export const coursesController = {
     // GET /courses/newest
     newest: async (req: Request, res: Response) => {
         try {
-        const newestCourses = await courseService.getTopTenNewest()
-        return res.json(newestCourses)
+            const newestCourses = await courseService.getTopTenNewest()
+            return res.json(newestCourses)
         } catch (err) {
-                if (err instanceof Error) {
-            return res.status(400).json({ message: err.message })
-        }
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
         }
     },
 
@@ -35,24 +38,30 @@ export const coursesController = {
             if (typeof name !== 'string') throw new Error('name param must be of type string');
             const courses = await courseService.findByName(name, page, perPage)
             return res.json(courses)
-            } catch (err) {
+        } catch (err) {
             if (err instanceof Error) {
                 return res.status(400).json({ message: err.message })
             }
         }
     },
 
-  // GET /courses/:id
-  show: async (req: Request, res: Response) => {
-    const { id } = req.params
+    // GET /courses/:id
+    show: async (req: AuthenticatedRequest, res: Response) => {
+        const userId = req.user!.id
+        const courseId = req.params.id
 
-    try {
-        const course = await courseService.findByIdWithEpisodes(id)
-        return res.json(course)
-      } catch (err) {
-        if (err instanceof Error) {
-          return res.status(400).json({ message: err.message })
+        try {
+            const course = await courseService.findByIdWithEpisodes(courseId)
+
+            if (!course) return res.status(404).json({ message: 'Curso não encontrado' })
+
+            const liked = await likeService.isLiked(userId, Number(courseId))
+            const favorited = await favoriteService.isFavorited(userId, Number(courseId))
+            return res.json({ ...course.get(), favorited, liked })
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })
+            }
         }
-      }
     }
 }
