85455997612ddf741bdfc0de01b42558163dee1c
Paula Rapkievicz <paularapkievicz@gmail.com>
2024-11-22 20:15:54 -0300

    Atualizando a senha

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -15,19 +15,6 @@ export const usersController = {
       }
     }
   },
-  // GET /users/current/watching
-  watching: async (req: AuthenticatedRequest, res: Response) => {
-    const { id } = req.user!
-
-    try {
-      const watching = await userService.getKeepWatchingList(id)
-      return res.json(watching)
-    } catch (err) {
-      if (err instanceof Error) {
-        return res.status(400).json({ message: err.message })
-      }
-    }
-  },
   // PUT /users/current
   update: async (req: AuthenticatedRequest, res: Response) => {
     const { id } = req.user!
@@ -43,6 +30,38 @@ export const usersController = {
       })
 
       return res.json(updatedUser)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+  // PUT /users/current/password
+  updatePassword: async (req: AuthenticatedRequest, res: Response) => {
+    const user = req.user!
+    const { currentPassword, newPassword } = req.body
+
+    user.checkPassword(currentPassword, async (err, isSame) => {
+      try {
+        if (err) return res.status(400).json({ message: err.message })
+        if (!isSame) return res.status(400).json({ message: "Senha incorreta" })
+
+        await userService.updatePassword(user.id, newPassword)
+        return res.status(204).send()
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+        }
+      }
+    })
+  },
+  // GET /users/current/watching
+  watching: async (req: AuthenticatedRequest, res: Response) => {
+    const { id } = req.user!
+
+    try {
+      const watching = await userService.getKeepWatchingList(id)
+      return res.json(watching)
     } catch (err) {
       if (err instanceof Error) {
         return res.status(400).json({ message: err.message })
